a2e589f3eae8849161f8ba6889e05536056cd921
josh@jmac <joshashep@gmail.com>
2019-03-29 22:03:05 -0700

    bash!

---
+++ pp2/run-all.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# note: suffix-tree may need to be re-written to run this
+
+for fname in data/*.{fas,fasta}; do
+    # the alphabet doesn't matter
+    ./bazel-bin/suffix-tree/suffix-tree fname data/DNA_alphabet.txt
+done
