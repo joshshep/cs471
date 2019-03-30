54943ddc647a2127ecbaacfb88c3ae069c3f9364
josh@jmac <joshashep@gmail.com>
2019-03-29 23:43:13 -0700

    output files

--- pp2/generate-bwt.sh
+++ pp2/generate-bwt.sh
@@ -8,10 +8,10 @@ mkdir -p $odir
 
 ofile="$odir/Tomato_BWT.txt"
 echo "Writing '$ofile' ..."
-./bazel-bin/suffix-tree/suffix-tree data/Slyco.fas data/DNA_alphabet.txt
+./bazel-bin/suffix-tree/suffix-tree data/Slyco.fas data/DNA_alphabet.txt > $ofile
 
 ofile="$odir/Yeast_BWT.txt"
 echo "Writing '$ofile' ..."
-./bazel-bin/suffix-tree/suffix-tree data/chr12.fas data/DNA_alphabet.txt
+./bazel-bin/suffix-tree/suffix-tree data/chr12.fas data/DNA_alphabet.txt > $ofile
 
 echo "done"
