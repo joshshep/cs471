ede14abf591e0450503ab61be68656e1987f590d
josh@jmac <joshashep@gmail.com>
2019-04-22 13:54:09 -0700

    better run file

--- pp3/run_peach.sh
+++ pp3/run_peach.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-./bazel-bin/read-map/read-map data/Peach_reference.fasta data/Peach_simulated_reads.fasta
+./bazel-bin/read-map/read-map data/Peach_reference.fasta data/Peach_simulated_reads_2.fasta
