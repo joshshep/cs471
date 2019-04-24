dde85891017f91e69cacaaba438b04244bd2faec
Surface <joshua.adam.shepherd@gmail.com>
2019-04-23 22:53:29 -0700

    spaces -> tabs

--- pp3/suffix-tree/suffix-tree-node.cc
+++ pp3/suffix-tree/suffix-tree-node.cc
@@ -3,130 +3,130 @@
 namespace suffix_tree {
 
 SuffixTreeNode* SuffixTreeNode::MatchStr(const char* query, int query_len, int &match_len) {
-    auto search = children_.find(query[0]);
-    if (search == children_.end()){
-        //match_len = 0;
-        return this;
-    }
-    auto child = search->second;
+	auto search = children_.find(query[0]);
+	if (search == children_.end()){
+		//match_len = 0;
+		return this;
+	}
+	auto child = search->second;
 
-    for (int i=0; i<child->edge_len_; i++){
-        if (i >= query_len){
-            // the query string ends in the middle of an edge, so we need to insert a node
-            match_len += i;
-            return child;
-        }
-        if (query[i] != child->incoming_edge_label_[i]){
-            // the edge labels do not match
-            // we need to create a new node
-            match_len += i;
-            return child;
-        }
-    }
+	for (int i=0; i<child->edge_len_; i++){
+		if (i >= query_len){
+			// the query string ends in the middle of an edge, so we need to insert a node
+			match_len += i;
+			return child;
+		}
+		if (query[i] != child->incoming_edge_label_[i]){
+			// the edge labels do not match
+			// we need to create a new node
+			match_len += i;
+			return child;
+		}
+	}
 
-    // we need to check this node's children
-    // recurse
-    match_len += child->edge_len_;
-    return child->MatchStr(query + child->edge_len_, query_len - child->edge_len_, match_len);
+	// we need to check this node's children
+	// recurse
+	match_len += child->edge_len_;
+	return child->MatchStr(query + child->edge_len_, query_len - child->edge_len_, match_len);
 }
 
 SuffixTreeNode* SuffixTreeNode::FindPath(const char* query, int query_len) {
-    //printf("FindPath('%*.*s')\n", query_len, query_len, query);
-    //assert(query_len > 0);
+	//printf("FindPath('%*.*s')\n", query_len, query_len, query);
+	//assert(query_len > 0);
 
-    auto search = children_.find(query[0]);
-    if (search == children_.end()){
-        // we need to add a child
-        auto newChild = new SuffixTreeNode(query, query_len, this);
-        children_[query[0]] = newChild;
-        return newChild;
-    }
-    auto child = search->second;
+	auto search = children_.find(query[0]);
+	if (search == children_.end()){
+		// we need to add a child
+		auto newChild = new SuffixTreeNode(query, query_len, this);
+		children_[query[0]] = newChild;
+		return newChild;
+	}
+	auto child = search->second;
 
-    for (int i=0; i<child->edge_len_; i++){
-        if (i >= query_len){
-            // the query string ends in the middle of an edge, so we need to insert a node
-            return this->InsertNode(child, query, query_len, i);
-        }
-        if (query[i] != child->incoming_edge_label_[i]){
-            // the edge labels do not match
-            // we need to create a new node
-            return this->InsertNode(child, query, query_len, i);
-        }
-    }
+	for (int i=0; i<child->edge_len_; i++){
+		if (i >= query_len){
+			// the query string ends in the middle of an edge, so we need to insert a node
+			return this->InsertNode(child, query, query_len, i);
+		}
+		if (query[i] != child->incoming_edge_label_[i]){
+			// the edge labels do not match
+			// we need to create a new node
+			return this->InsertNode(child, query, query_len, i);
+		}
+	}
 
-    // we're at the end of the edge
-    if (child->edge_len_ == query_len){
-        // this suffix is already in the the tree
-        // nothing to do
-        // when does this happen?
-        printf("error: suffix already exists in the tree?!\n");
-        assert(0);
-        exit(1);
-        return nullptr;
-    }
+	// we're at the end of the edge
+	if (child->edge_len_ == query_len){
+		// this suffix is already in the the tree
+		// nothing to do
+		// when does this happen?
+		printf("error: suffix already exists in the tree?!\n");
+		assert(0);
+		exit(1);
+		return nullptr;
+	}
 
-    // we need to check this node's children
-    // recurse
-    //printf("recurse FindPath(%*.*s)\n", query_len - child->edge_len_, query_len - child->edge_len_, query + child->edge_len_);
-    return child->FindPath(query + child->edge_len_, query_len - child->edge_len_);
+	// we need to check this node's children
+	// recurse
+	//printf("recurse FindPath(%*.*s)\n", query_len - child->edge_len_, query_len - child->edge_len_, query + child->edge_len_);
+	return child->FindPath(query + child->edge_len_, query_len - child->edge_len_);
 }
 
 SuffixTreeNode* SuffixTreeNode::NodeHops(const char* beta, int beta_len) {
-    //printf("NodeHops(%*.*s)\n", beta_len, beta_len, beta);
-    if (beta_len==0) {
-        //printf("beta_len == 0\n");
-        return this;
-    }
-    auto search = children_.find(beta[0]);
-    assert(search != children_.end());
-    auto child = search->second;
-    assert(child);
+	//printf("NodeHops(%*.*s)\n", beta_len, beta_len, beta);
+	if (beta_len==0) {
+		//printf("beta_len == 0\n");
+		return this;
+	}
+	auto search = children_.find(beta[0]);
+	assert(search != children_.end());
+	auto child = search->second;
+	assert(child);
 
-    if (beta_len > child->edge_len_){
-        // hop
-        //printf("NodeHops(): hop to next node\n");
-        return child->NodeHops(beta + child->edge_len_, beta_len - child->edge_len_);
-    }
-    
-    if (beta_len == child->edge_len_) {
-        // the node exists
-        //printf("NodeHops(): the node simply already exists\n");
-        return child;
-    }
+	if (beta_len > child->edge_len_){
+		// hop
+		//printf("NodeHops(): hop to next node\n");
+		return child->NodeHops(beta + child->edge_len_, beta_len - child->edge_len_);
+	}
+	
+	if (beta_len == child->edge_len_) {
+		// the node exists
+		//printf("NodeHops(): the node simply already exists\n");
+		return child;
+	}
 
-    // we need to break the edge and create a node
+	// we need to break the edge and create a node
 
-    //printf("NodeHops(): need to break an edge\n");
-    return this->BreakEdge(child, beta_len);
+	//printf("NodeHops(): need to break an edge\n");
+	return this->BreakEdge(child, beta_len);
 }
 
 SuffixTreeNode* SuffixTreeNode::BreakEdge(SuffixTreeNode* child, int index) {
-    assert(index > 0);
-    assert(index < child->edge_len_);
-    
-    auto newInternalNode = new SuffixTreeNode(child->incoming_edge_label_, index, this);
-    newInternalNode->children_[child->incoming_edge_label_[index]] = child;
+	assert(index > 0);
+	assert(index < child->edge_len_);
+	
+	auto newInternalNode = new SuffixTreeNode(child->incoming_edge_label_, index, this);
+	newInternalNode->children_[child->incoming_edge_label_[index]] = child;
 
-    
-    child->parent_ = newInternalNode;
-    child->incoming_edge_label_ += index;
-    child->edge_len_ -= index;
+	
+	child->parent_ = newInternalNode;
+	child->incoming_edge_label_ += index;
+	child->edge_len_ -= index;
 
-    // this -> newInternalNode -> child
-    return newInternalNode;
+	// this -> newInternalNode -> child
+	return newInternalNode;
 }
 
 SuffixTreeNode* SuffixTreeNode::InsertNode(SuffixTreeNode* child, const char* query, int query_len, int index) {
-    assert(index > 0);
-    //edge_len_ -= index;
-    //if (ed)
-    //assert(edge_len_ > 0);
+	assert(index > 0);
+	//edge_len_ -= index;
+	//if (ed)
+	//assert(edge_len_ > 0);
 
-    auto newInternalNode = this->BreakEdge(child, index);
-    auto newLeafNode = new SuffixTreeNode(query + index, query_len - index, newInternalNode);
+	auto newInternalNode = this->BreakEdge(child, index);
+	auto newLeafNode = new SuffixTreeNode(query + index, query_len - index, newInternalNode);
 
-    return newLeafNode;
+	return newLeafNode;
 }
 
 } // namespace suffix_tree
--- pp3/suffix-tree/suffix-tree.h
+++ pp3/suffix-tree/suffix-tree.h
@@ -11,116 +11,116 @@ namespace suffix_tree {
 
 class SuffixTree {
 public:
-    SuffixTree(const char* str, int len) : str_(str), len_(len) {
-        //BuildTreeSimple();
-        printf("input str length: %d\n", len);
-        printf("Building suffix tree...\n");
+	SuffixTree(const char* str, int len) : str_(str), len_(len) {
+		//BuildTreeSimple();
+		printf("input str length: %d\n", len);
+		printf("Building suffix tree...\n");
 
-        // simply build the tree
-        BuildTreeMccreight();
+		// simply build the tree
+		BuildTreeMccreight();
 
-        // build the tree with timing and stats
-        //PrintBuildStats();
-    }
-    ~SuffixTree(){
-        delete root_;
-    }
+		// build the tree with timing and stats
+		//PrintBuildStats();
+	}
+	~SuffixTree(){
+		delete root_;
+	}
 
-    /*
-    SuffixTreeNode* MatchStr(const char* query, int query_len, int & match_len) {
-        return root_->MatchStr()
-    }
-    */
+	/*
+	SuffixTreeNode* MatchStr(const char* query, int query_len, int & match_len) {
+		return root_->MatchStr()
+	}
+	*/
 
-    // returns the number of nodes in the tree
-    int BuildTreeMccreight();
+	// returns the number of nodes in the tree
+	int BuildTreeMccreight();
 
-    void PrintBuildStats() {
-        // time build
-        auto t1 = std::chrono::high_resolution_clock::now();
-        int num_nodes = BuildTreeMccreight();
-        auto t2 = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
+	void PrintBuildStats() {
+		// time build
+		auto t1 = std::chrono::high_resolution_clock::now();
+		int num_nodes = BuildTreeMccreight();
+		auto t2 = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
 
-        // print stats
-        printf("Time elapsed while building suffix tree: %llu microsecond(s)\n", duration);
-        printf("total number of nodes in the tree: %d\n", num_nodes);
-        printf("number of internal nodes in the tree: %d\n", num_nodes - len_);
-        auto total_str_depth = root_->TotalStrDepth();
-        double avg_depth = (double) total_str_depth / (num_nodes - len_);
-        printf("avg str depth of internal node: %lf\n", avg_depth);
-    }
-    void PrintPath(SuffixTreeNode *descendant) {
-        if (descendant)
-            PrintPath(root_, descendant);
-    }
-    void PrintPath(SuffixTreeNode *ancestor, SuffixTreeNode *descendant) {
-        //printf("\n[%d] is the parent of [%d]\n", descendant->parent_->id_, descendant->id_);
-        //printf("\nPrintPath(parent_id=[%d], child_id=[%d]): ", ancestor->id_, descendant->id_);
-        if (ancestor == descendant) {
-            //printf("FOUND ancestor from desendent ; str_depth_=%d ; edge_len=%d\n", descendant->str_depth_, descendant->edge_len_);
-            return;
-        }
-        //printf("str_depth_=%d ; chars to print=%d", descendant->str_depth_, descendant->edge_len_);
-        PrintPath(ancestor, descendant->parent_);
-        printf("%*.*s", descendant->edge_len_, descendant->edge_len_, descendant->incoming_edge_label_);
-    }
+		// print stats
+		printf("Time elapsed while building suffix tree: %llu microsecond(s)\n", duration);
+		printf("total number of nodes in the tree: %d\n", num_nodes);
+		printf("number of internal nodes in the tree: %d\n", num_nodes - len_);
+		auto total_str_depth = root_->TotalStrDepth();
+		double avg_depth = (double) total_str_depth / (num_nodes - len_);
+		printf("avg str depth of internal node: %lf\n", avg_depth);
+	}
+	void PrintPath(SuffixTreeNode *descendant) {
+		if (descendant)
+			PrintPath(root_, descendant);
+	}
+	void PrintPath(SuffixTreeNode *ancestor, SuffixTreeNode *descendant) {
+		//printf("\n[%d] is the parent of [%d]\n", descendant->parent_->id_, descendant->id_);
+		//printf("\nPrintPath(parent_id=[%d], child_id=[%d]): ", ancestor->id_, descendant->id_);
+		if (ancestor == descendant) {
+			//printf("FOUND ancestor from desendent ; str_depth_=%d ; edge_len=%d\n", descendant->str_depth_, descendant->edge_len_);
+			return;
+		}
+		//printf("str_depth_=%d ; chars to print=%d", descendant->str_depth_, descendant->edge_len_);
+		PrintPath(ancestor, descendant->parent_);
+		printf("%*.*s", descendant->edge_len_, descendant->edge_len_, descendant->incoming_edge_label_);
+	}
 
-    // SL(u) is known (root or internal node)
-    SuffixTreeNode* Case1(SuffixTreeNode* prev_leaf, int index);
+	// SL(u) is known (root or internal node)
+	SuffixTreeNode* Case1(SuffixTreeNode* prev_leaf, int index);
 
-    // SL(u) is _not_ known (root or internal node)
-    SuffixTreeNode* Case2(SuffixTreeNode* prev_leaf);
+	// SL(u) is _not_ known (root or internal node)
+	SuffixTreeNode* Case2(SuffixTreeNode* prev_leaf);
 
-    void BuildTreeSimple(){
-        root_ = new SuffixTreeNode(str_, len_, nullptr);
-        for (int i=0; i<len_-1; i++){
-            root_->FindPath(str_+i, len_-i);
-        }
-    }
+	void BuildTreeSimple(){
+		root_ = new SuffixTreeNode(str_, len_, nullptr);
+		for (int i=0; i<len_-1; i++){
+			root_->FindPath(str_+i, len_-i);
+		}
+	}
 
-    void PrintTree(){
-        root_->PrintChildren();
-    }
+	void PrintTree(){
+		root_->PrintChildren();
+	}
 
-    void FindPath();
+	void FindPath();
 
-    void GetLongestMatchingRepeat() {
-        SuffixTreeNode * n = root_->GetDeepestInternalNode();
-        printf("string depth of deepest node: %d\n", n->str_depth_);
-        printf("indices of longest exact matching repeat:");
-        for (auto child : n->children_) {
-            assert(child.second->IsLeaf());
-            printf(" %d", child.second->id_);
-        }
-        printf("\n");
-    }
+	void GetLongestMatchingRepeat() {
+		SuffixTreeNode * n = root_->GetDeepestInternalNode();
+		printf("string depth of deepest node: %d\n", n->str_depth_);
+		printf("indices of longest exact matching repeat:");
+		for (auto child : n->children_) {
+			assert(child.second->IsLeaf());
+			printf(" %d", child.second->id_);
+		}
+		printf("\n");
+	}
 
-    void NodeHops();
+	void NodeHops();
 
-    SuffixTreeNode* root_;
-    const char* str_;
-    int len_;
+	SuffixTreeNode* root_;
+	const char* str_;
+	int len_;
 
 
-    void PrintBWTindex(){
-        PrintBWTindex(root_);
-    }
+	void PrintBWTindex(){
+		PrintBWTindex(root_);
+	}
 
-    void PrintBWTindex(SuffixTreeNode* n){
-        if (n->children_.size() == 0) {
-            // Why not just zero index? ugh ...
-            if (n->id_==0) {
-                printf("%c\n", str_[len_-1]);
-            } else {
-                printf("%c\n", str_[n->id_-1]);
-            }
-            return;
-        }
-        for (auto child : n->children_){
-            PrintBWTindex(child.second);
-        }
-    }
+	void PrintBWTindex(SuffixTreeNode* n){
+		if (n->children_.size() == 0) {
+			// Why not just zero index? ugh ...
+			if (n->id_==0) {
+				printf("%c\n", str_[len_-1]);
+			} else {
+				printf("%c\n", str_[n->id_-1]);
+			}
+			return;
+		}
+		for (auto child : n->children_){
+			PrintBWTindex(child.second);
+		}
+	}
 
 };
 
