c050dd4ab014ef1f2a6379f32896a1a059235268
josh@jmac <joshashep@gmail.com>
2019-05-21 10:40:29 -0700

    rm bad check for null terminated genomes

--- pp3/suffix-tree/suffix-tree-node.cc
+++ pp3/suffix-tree/suffix-tree-node.cc
@@ -5,7 +5,6 @@ namespace suffix_tree {
 SuffixTreeNode* SuffixTreeNode::MatchStr(const char* query, int query_len, int &match_len) {
 	auto search = children_.find(query[0]);
 	if (search == children_.end()){
-		//match_len = 0;
 		return this;
 	}
 	auto child = search->second;
@@ -29,16 +28,11 @@ SuffixTreeNode* SuffixTreeNode::MatchStr(const char* query, int query_len, int &
 	match_len += child->edge_len_;
 	return child->MatchStr(query + child->edge_len_, query_len - child->edge_len_, match_len);
 }
-
 SuffixTreeNode* SuffixTreeNode::FindPath(const char* query, int query_len) {
-	//printf("FindPath('%*.*s')\n", query_len, query_len, query);
-	//assert(query_len > 0);
-
 	auto search = children_.find(query[0]);
 	if (search == children_.end()){
 		// we need to add a child
 		auto newChild = new SuffixTreeNode(query, query_len, this);
-		children_[query[0]] = newChild;
 		return newChild;
 	}
 	auto child = search->second;
@@ -46,6 +40,7 @@ SuffixTreeNode* SuffixTreeNode::FindPath(const char* query, int query_len) {
 	for (int i=0; i<child->edge_len_; i++){
 		if (i >= query_len){
 			// the query string ends in the middle of an edge, so we need to insert a node
+			// TODO ??
 			return this->InsertNode(child, query, query_len, i);
 		}
 		if (query[i] != child->incoming_edge_label_[i]){
@@ -55,6 +50,7 @@ SuffixTreeNode* SuffixTreeNode::FindPath(const char* query, int query_len) {
 		}
 	}
 
+	/*
 	// we're at the end of the edge
 	if (child->edge_len_ == query_len){
 		// this suffix is already in the the tree
@@ -65,17 +61,15 @@ SuffixTreeNode* SuffixTreeNode::FindPath(const char* query, int query_len) {
 		exit(1);
 		return nullptr;
 	}
+	*/
 
 	// we need to check this node's children
 	// recurse
-	//printf("recurse FindPath(%*.*s)\n", query_len - child->edge_len_, query_len - child->edge_len_, query + child->edge_len_);
 	return child->FindPath(query + child->edge_len_, query_len - child->edge_len_);
 }
 
 SuffixTreeNode* SuffixTreeNode::NodeHops(const char* beta, int beta_len) {
-	//printf("NodeHops(%*.*s)\n", beta_len, beta_len, beta);
 	if (beta_len==0) {
-		//printf("beta_len == 0\n");
 		return this;
 	}
 	auto search = children_.find(beta[0]);
@@ -85,15 +79,28 @@ SuffixTreeNode* SuffixTreeNode::NodeHops(const char* beta, int beta_len) {
 
 	if (beta_len < child->edge_len_) {
 		// we need to break the edge and create a node
-		//printf("NodeHops(): need to break an edge\n");
 		return this->BreakEdge(child, beta_len);
 	}
 
 	// hop
-	//printf("NodeHops(): hop to next node\n");
 	return child->NodeHops(beta + child->edge_len_, beta_len - child->edge_len_);
 }
 
+/*
+this
+  \
+   \
+    \
+   child
+
+this
+  \
+   \
+  newInternalNode
+        \
+         \
+        child
+*/
 SuffixTreeNode* SuffixTreeNode::BreakEdge(SuffixTreeNode* child, int index) {
 	assert(index > 0);
 	assert(index < child->edge_len_);
@@ -110,11 +117,32 @@ SuffixTreeNode* SuffixTreeNode::BreakEdge(SuffixTreeNode* child, int index) {
 	return newInternalNode;
 }
 
+/*
+this
+  \
+   \
+    \
+   child
+
+this
+  \
+   \
+  newInternalNode
+        \
+         \
+        child
+
+this
+  \
+   \
+  newInternalNode
+    /       \
+   /         \
+child      newLeafNode
+*/
 SuffixTreeNode* SuffixTreeNode::InsertNode(SuffixTreeNode* child, const char* query, int query_len, int index) {
 	assert(index > 0);
-	//edge_len_ -= index;
-	//if (ed)
-	//assert(edge_len_ > 0);
+	assert(index < query_len);
 
 	auto newInternalNode = this->BreakEdge(child, index);
 	auto newLeafNode = new SuffixTreeNode(query + index, query_len - index, newInternalNode);
