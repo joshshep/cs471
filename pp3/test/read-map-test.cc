f75eb79a30eb106ae19b7e59f478a66e09fdc207
josh@jmac <joshashep@gmail.com>
2019-05-21 23:41:30 -0700

    impl readmap bad match test

--- pp3/test/read-map-test.cc
+++ pp3/test/read-map-test.cc
@@ -23,3 +23,25 @@ TEST(ReadMap, Sample1) {
 	delete read_mapping;
 	delete st;
 }
+
+TEST(ReadMap, Sample2) {
+	std::string genome = "bcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzbcdefghijklmnopqrstuvwxyz";
+	std::string read = "aaaaaaaaaaaa";
+
+	suffix_tree::Sequence genome_seq = {"genome", genome};
+	suffix_tree::Sequence read_seq = {"read12", read};
+	auto st = new suffix_tree::SuffixTree(genome.c_str(), genome.size());
+
+	std::vector<read_map::Sequence> reads{read_seq};
+	const aligner::ScoreConfig align_config = {1, -2, -5, -1};
+	read_map::ReadMap * read_mapping = new read_map::ReadMap(genome_seq, reads, align_config);
+	int next_index = 0;
+	read_mapping->PrepareST(st->root_, next_index);
+
+	// TODO
+	auto strpos = read_mapping->CalcReadMapping(read_seq);
+	EXPECT_EQ(-1, strpos.start);
+	EXPECT_EQ(-1, strpos.len);
+	delete read_mapping;
+	delete st;
+}
