105a411b0742c99930e8c4e80b97f75a3d4ce0dc
josh@jmac <joshashep@gmail.com>
2019-05-21 22:00:30 -0700

    impl abc test for readmap (segfaults)

--- pp3/test/read-map-test.cc
+++ pp3/test/read-map-test.cc
@@ -2,34 +2,24 @@
 
 #include "read-map/read-map.h"
 
-/*
 TEST(ReadMap, Sample1) {
-	std::string genome = "bcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzbcdefghijklmnopqrstuvwxyz$";
+	std::string genome = "bcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzbcdefghijklmnopqrstuvwxyz";
 	std::string read = "abcdefghijklmnopqrstuvwxyz";
 
-	read_map::Sequence genome_seq = {"genome", genome};
-	read_map::Sequence read_seq = {"read25", read};
+	suffix_tree::Sequence genome_seq = {"genome", genome};
+	suffix_tree::Sequence read_seq = {"read26", read};
+	auto st = new suffix_tree::SuffixTree(genome.c_str(), genome.size());
+
 	std::vector<read_map::Sequence> reads{read_seq};
-	read_map::ReadMap * read_mapping = new read_map::ReadMap(genome_seq, reads);
-	read_mapping->Run();
-	EXPECT_EQ(0, 0);
-}
-*/
+	const aligner::ScoreConfig align_config = {1, -2, -5, -1};
+	read_map::ReadMap * read_mapping = new read_map::ReadMap(genome_seq, reads, align_config);
+	int next_index = 0;
+	read_mapping->PrepareST(st->root_, next_index);
 
-/*
-TEST(SuffixTree, Sample1) {
-	std::string genome = "banana$";
-	std::string read = "ana";
-
-	int match_len = 0;
-	suffix_tree::SuffixTree* st = new suffix_tree::SuffixTree(genome.c_str(), genome.size());
-	auto match_node = st->root_->MatchStr(read.c_str(), read.size(), match_len);
-	printf("match_len = %d\n", match_len);
-	st->PrintPath(match_node);
-	printf("\n");
-	st->PrintTree();
-	st->root_->PrintParentage();
+	// TODO
+	auto strpos = read_mapping->CalcReadMapping(read_seq);
+	EXPECT_EQ(25, strpos.start);
+	EXPECT_EQ(26, strpos.len);
+	delete read_mapping;
 	delete st;
-	EXPECT_EQ(0, 0);
 }
-*/
