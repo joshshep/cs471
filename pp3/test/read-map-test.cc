c11e8f5f6425d5ccd52e2b02b44e651d7e841799
josh@jmac <joshashep@gmail.com>
2019-05-30 22:08:29 -0700

    fix tests for readmapworker

--- pp3/test/read-map-test.cc
+++ pp3/test/read-map-test.cc
@@ -2,34 +2,60 @@
 
 #include "read-map/read-map.h"
 
-TEST(ReadMap, Sample1) {
+TEST(ReadMapWorker, Sample1) {
 	std::string genome = "bcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzbcdefghijklmnopqrstuvwxyz";
 	std::string read = "abcdefghijklmnopqrstuvwxyz";
 
 	suffix_tree::Sequence genome_seq = {"genome", genome};
 	suffix_tree::Sequence read_seq = {"read26", read};
 
+	// build the suffix tree
+	suffix_tree::SuffixTree* st = new suffix_tree::SuffixTree(genome_seq.bps.c_str(), genome_seq.bps.size());
+	st->BuildTreeMccreight();
+
+	// build A
+	int next_index = 0;
+	int * A = new int[genome_seq.bps.size()];
+	read_map::ReadMap::PrepareST(A, st->root_, next_index);
+
 	std::vector<read_map::Sequence> reads{read_seq};
 	const aligner::ScoreConfig align_config = {1, -2, -5, -1};
-	read_map::ReadMap * read_mapping = new read_map::ReadMap(genome_seq, reads, align_config);
+	read_map::ReadMapWorker * worker = new read_map::ReadMapWorker(0, genome_seq, reads, align_config, *st, A);
 
-	read_mapping->Run();
+	auto pos = worker->CalcReadMapping(read_seq);
+	EXPECT_EQ(25, pos.start);
+	EXPECT_EQ(26, pos.len);
 
-	delete read_mapping;
+	delete worker;
+	delete[] A;
+	delete st;
 }
 
-TEST(ReadMap, Sample2) {
+TEST(ReadMapWorker, Sample2) {
 	std::string genome = "bcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzbcdefghijklmnopqrstuvwxyz";
 	std::string read = "aaaaaaaaaaaa";
 
 	suffix_tree::Sequence genome_seq = {"genome", genome};
 	suffix_tree::Sequence read_seq = {"read12", read};
 
+	// build the suffix tree
+	suffix_tree::SuffixTree* st = new suffix_tree::SuffixTree(genome_seq.bps.c_str(), genome_seq.bps.size());
+	st->BuildTreeMccreight();
+
+	// build A
+	int next_index = 0;
+	int * A = new int[genome_seq.bps.size()];
+	read_map::ReadMap::PrepareST(A, st->root_, next_index);
+
 	std::vector<read_map::Sequence> reads{read_seq};
 	const aligner::ScoreConfig align_config = {1, -2, -5, -1};
-	read_map::ReadMap * read_mapping = new read_map::ReadMap(genome_seq, reads, align_config);
+	read_map::ReadMapWorker * worker = new read_map::ReadMapWorker(0, genome_seq, reads, align_config, *st, A);
 
-	read_mapping->Run();
+	auto pos = worker->CalcReadMapping(read_seq);
+	EXPECT_EQ(-1, pos.start);
+	EXPECT_EQ(-1, pos.len);
 
-	delete read_mapping;
+	delete worker;
+	delete[] A;
+	delete st;
 }
