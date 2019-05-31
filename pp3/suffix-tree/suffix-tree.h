4c26efcb6d40708213c5c9291cb5a25fc027ae5f
josh@jmac <joshashep@gmail.com>
2019-05-31 02:53:42 -0500

    cleanup

--- pp3/suffix-tree/suffix-tree.h
+++ pp3/suffix-tree/suffix-tree.h
@@ -116,38 +116,6 @@ public:
 
 };
 
-////////////////////////////////
-// printing 
-////////////////////////////////
-
-// prints the help to run this executable 
-void PrintHelp();
-
-// pretty-prints the size value as quantity of {B, KiB, MiB, GiB}
-void PrintSize(size_t asize);
-
-
-///////////////////////////////////////
-// loading from files
-///////////////////////////////////////
-
-// loads exactly 1 sequence from the provided file name
-Sequence LoadSequence(const char* fasta_fname);
-
-
-///////////////////////////////////////////
-// misc free helpers (TODO)
-///////////////////////////////////////////
-
-// formats the input string (representing base pairs) to lowercase
-void Format_bps(std::string & str);
-
-// trims the provided whitespace characters from the provided string
-std::string Trim(const std::string& str, const std::string& whitespace = " ");
-
-// determines the length of the input integer
-char intLen(int n);
-
 } // namespace suffix_tree
 
 #endif
